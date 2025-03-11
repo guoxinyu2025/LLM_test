898dc64c16f84bfabce028919f1aadb3326d0cea
guoxinyu.vendor <1016355431@qq.com>
2025-03-11 12:13:52 +0800

    主函数

--- API_Master/API_stress.py
+++ API_Master/API_stress.py
@@ -1,14 +1,14 @@
 # main.py
-import api_config  # 导入api_requests列表
+import api_config
 import request_sender
 
 # 调用所有接口
 if __name__ == "__main__":
     for request in api_config.api_requests:
-        method = request.get("method", "POST").upper()  # 默认是POST，可以从配置中获取
-        params = request.get("params")  # GET请求的参数
-        data = request.get("data")  # POST请求的数据
-        files = request.get("files")  # 文件上传
+        method = request.get("method", "POST").upper()  # 默认是POST，可从配置中获取
+        params = request.get("params")
+        data = request.get("data")
+        files = request.get("files")
         request_sender.send_request(
             request["url"],
             request["headers"],
