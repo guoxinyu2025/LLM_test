6d1202cea09bd68f6f67b9338f31a7af1e73c56b
guoxinyu.vendor <1016355431@qq.com>
2025-03-11 14:29:11 +0800

    主函数

--- API_Master/API_stress.py
+++ API_Master/API_stress.py
@@ -1,10 +1,10 @@
-# main.py
 import api_config
 import request_sender
 
 # 调用所有接口
 if __name__ == "__main__":
     for request in api_config.api_requests:
+        api_name = request.get("name")  # 获取接口名称
         method = request.get("method", "POST").upper()  # 默认是POST，可从配置中获取
         params = request.get("params")
         data = request.get("data")
@@ -12,6 +12,7 @@ if __name__ == "__main__":
         request_sender.send_request(
             request["url"],
             request["headers"],
+            api_name,  # 传递接口名称
             method=method,
             params=params,
             data=data,
--- API_Master/request_sender.py
+++ API_Master/request_sender.py
@@ -1,21 +1,20 @@
-# request_sender.py
 import requests
 import json
 
-# 通用请求函数
-def send_request(url, headers, method="POST", params=None, data=None, files=None):
+# 通用请求函数，增加 api_name 参数
+def send_request(url, headers, api_name, method="POST", params=None, data=None, files=None):
     try:
         if method.upper() == "GET":
             response = requests.get(url, headers=headers, params=params)
         else:
             response = requests.post(url, headers=headers, data=json.dumps(data) if data else None, files=files)
         response.raise_for_status()
-        print("Request succeeded")
+        print(f"{api_name}# Request succeeded")
     except requests.exceptions.HTTPError as errh:
-        print(f"Http Error: {errh}")
+        print(f"{api_name}# Http Error: {errh}")
     except requests.exceptions.ConnectionError as errc:
-        print(f"Error Connecting: {errc}")
+        print(f"{api_name}# Error Connecting: {errc}")
     except requests.exceptions.Timeout as errt:
-        print(f"Timeout Error: {errt}")
+        print(f"{api_name}# Timeout Error: {errt}")
     except requests.exceptions.RequestException as err:
-        print(f"Oops: Something Else {err}")
+        print(f"{api_name}# Oops: Something Else {err}")
